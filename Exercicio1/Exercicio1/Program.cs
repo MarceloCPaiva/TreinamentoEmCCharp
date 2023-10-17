f55a9886dfd4e34efba679360851ade5fee27d0c
Marcelo Paiva <marcelochagas.paiva@gmail.com>
2023-10-17 20:26:11 -0300

    Primeiro commit

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Treinando Entrada e saída de dados
+
+using System.Globalization;
+
+int quartos, idade;
+string nomeCompleto, ultimoNome;
+double precoProduto, altura;
+
+Console.WriteLine("Entre com o seu nome completo: ");
+nomeCompleto = Console.ReadLine();
+
+Console.WriteLine("Quantos quartos tem na sua casa? ");
+quartos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o preço de um produto: ");
+precoProduto = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com o seu último nome, idade e altura (na mesma linha) ");
+string[] vet = Console.ReadLine().Split(' ');
+
+ultimoNome = vet[0];
+idade = int.Parse(vet[1]);
+altura = double.Parse(vet[2]);
+
+Console.WriteLine(nomeCompleto);
+Console.WriteLine(quartos);
+Console.WriteLine(precoProduto.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine(ultimoNome);
+Console.WriteLine(idade);
+Console.WriteLine(altura.ToString("F2",CultureInfo.InvariantCulture));
---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,81 @@
+﻿// Programa para ler as medidas dos lados de dois triângulos X e Y.
+// Em seguida mostrar o valor das áreas dos dois triângulos.
+// Dizer qual dos dois triângulos possui a maior área.
+
+//Fórmula para calcular a área A, B e C:
+// area = raiz quadradada de P(P-A)(P-B)(P-C) onde P= A+B+C / 2
+
+//Programa se Classe
+
+//using System.Globalization;
+
+//double xA, xB, xC, yA, yB, yC, pX, areaX, pY, areaY;
+
+//Console.WriteLine("Entre com as medidas do triângulo X: ");
+//xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//Console.WriteLine("Entre com as medidas do triângulo Y: ");
+//yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//pX = (xA + xB + xC) / 2;   
+//areaX = Math.Sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
+
+//pY = (yA + yB + yC) / 2;
+//areaY = Math.Sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+
+//Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+//Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+//if  (areaX > areaY)
+//{
+//    Console.WriteLine("Maior área: X");
+//}
+//else
+//{
+//    Console.WriteLine("Maior área: Y");
+
+//}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Programa com Classe
+
+using Exercicio2;
+using System.Globalization;
+
+Triangulo x, y;
+
+x = new Triangulo();
+y = new Triangulo();
+
+Console.WriteLine("Entre com as medidas do triângulo X: ");
+x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com as medidas do triângulo Y: ");
+y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double p = (x.A + x.B + x.C) / 2;
+double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+p = (y.A + y.B + y.C) / 2;
+double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+if (areaX > areaY)
+{
+    Console.WriteLine("Maior área: X");
+}
+else
+{
+    Console.WriteLine("Maior área: Y");
+
+}
