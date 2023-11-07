60f6fbd27faa62d02b59fc3bf5308b26f29221eb
Marcelo Paiva <marcelochagas.paiva@gmail.com>
2023-11-07 16:31:44 -0300

    Alterando Exercício 5 para construtor e sobrecarga

--- Exercicio5/Exercicio5/Produto.cs
+++ Exercicio5/Exercicio5/Produto.cs
@@ -8,6 +8,12 @@ namespace Exercicio5
         public double Preco;
         public int Quantidade;
 
+        public Produto(string nome, double preco, int quantidade) 
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
         public double ValorTotalEmEstoque()
         {
             return Preco * Quantidade;
