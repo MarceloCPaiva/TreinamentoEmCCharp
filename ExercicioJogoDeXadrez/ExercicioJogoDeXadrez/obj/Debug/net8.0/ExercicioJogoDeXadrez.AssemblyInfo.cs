0ca08efbf99e55e6a51665a78bcab6d8441acc1b
Marcelo Paiva <marcelochagas.paiva@gmail.com>
2024-03-14 20:50:46 -0300

    Controlando as peças capturadas.

--- ExercicioJogoDeXadrez/ExercicioJogoDeXadrez/obj/Debug/net8.0/ExercicioJogoDeXadrez.AssemblyInfo.cs
+++ ExercicioJogoDeXadrez/ExercicioJogoDeXadrez/obj/Debug/net8.0/ExercicioJogoDeXadrez.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercicioJogoDeXadrez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc68e37180ce01aa56803f33dca90c4355d39314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941a8f4c4d36a50fc6733cad3e469b41fdb6f2e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercicioJogoDeXadrez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExercicioJogoDeXadrez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
